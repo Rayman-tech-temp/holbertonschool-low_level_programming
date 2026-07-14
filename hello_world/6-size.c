c13e7adf8cb0939b11182a052162dc19893aa955
Ray Callegari <14826@holbertonstudents.com>
2026-07-14 01:35:22 +0000

    Answer to size is not grandure code take two

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -23,7 +23,7 @@ int main(void)
 	 * prints data size of the variables in bytes.
 	 */
 	printf("Size of a char: %d byte(s)\n", sizeof(charVar));
-	printf("Size of a int: %d byte(s)\n", sizeof(intVar));
+	printf("Size of an int: %d byte(s)\n", sizeof(intVar));
 	printf("Size of a long int: %d byte(s)\n", sizeof(longVar));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(longLongVar));
 	printf("Size of a float: %d byte(s)\n", sizeof(floatVar));
