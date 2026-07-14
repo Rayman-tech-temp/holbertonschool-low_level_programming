b2ea91781b616ad78ed4d6616ef1fb9f9cad7f6c
Ray Callegari <14826@holbertonstudents.com>
2026-07-14 01:08:05 +0000

    Answer hello printf code

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This is to demostrate the printf function in the
+ * stdio.h library by having new lines after every comma
+ * using the \n code in the string.
+ *
+ * Return: 0 as it is a simple excerise of running
+ * functions for beginner programming - always SUCCESS
+ */
+
+int main(void)
+{
+	printf("with proper grammar,\n but the outcome is a piece of art,\n");
+	return (0);
+}
