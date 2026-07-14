15e9252190126e4a998ee2a8483f34d23d8a041d
Ray Callegari <14826@holbertonstudents.com>
2026-07-14 00:40:59 +0000

    Answer hello puts code take five

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * The puts function is a stdio.h function that outputs
+ * to the console that can be configured to point to a file or other location
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle.");
