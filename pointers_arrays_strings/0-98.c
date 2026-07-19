e4d37f26f99f1dbc7a450c4335138a0d983db0af
Ray Callegari <14826@holbertonstudents.com>
2026-07-19 03:57:42 +0000

    Answering the 98 task

---
+++ pointers_arrays_strings/0-98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+void reset_to_98(int* ptr)
+{
+	*ptr = 98;
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
