9edb6805c6523cabedf87049f563af2bfe9cb4bf
Ray Callegari <14826@holbertonstudents.com>
2026-07-20 05:06:04 +0000

    Setup + Skeleton Program take one

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the interactive terminal interface.
+ *
+ * Return:
+ */
+
+void main(void)
+{
+	int input;
+
+	do {
+		printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\nChoice:");
+		scanf("%d", input);
+	} while (input != 0);
+	return (0);
+}
