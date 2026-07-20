78c9ee3eb56fc8d73f67f5bd5bf478b3e6a2e129
Ray Callegari <14826@holbertonstudents.com>
2026-07-20 02:36:47 +0000

    adding in 0-reset_to_98 file after being removed

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - using a pointer to change
+ * the value of a variable pass in this
+ * function.
+ *
+ * @ptr: a passed variable that holds the ddress to alter
+ * the held value there
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *ptr)
+{
+        *ptr = 98;
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+        int n;
+
+        n = 402;
+        printf("n=%d\n", n);
+        reset_to_98(&n);
+        printf("n=%d\n", n);
+        return (0);
+}
