c14cabc3fef55269dcd73f8aeb19d79bb3bc6936
Ray Callegari <14826@holbertonstudents.com>
2026-07-20 04:08:49 +0000

    setting up the evironment to run C pointers arrays and strings project

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 
 /**
  * reset_to_98 - using a pointer to change
@@ -14,21 +13,4 @@
 void reset_to_98(int *ptr)
 {
         *ptr = 98;
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
 }
