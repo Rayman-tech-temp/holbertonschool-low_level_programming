c65ea5a2011c4bc63d0cd7e82d1fe86bc114a374
Ray Callegari <14826@holbertonstudents.com>
2026-07-20 05:32:17 +0000

    Menue Loop + Choice Validation take one

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -8,11 +8,19 @@
 
 int main(void)
 {
-	int input=9;
+	int input = 9;
 
+	printf("Welcome to Simple Calculator!\n");
 	do {
 		printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\nChoice:");
 		scanf("%d", &input);
+		if (input > 4)
+		{
+			printf("Invalid choice\n");
+		} else if (input == 0)
+		{
+			printf("Bye!");
+		}
 	} while (input != 0);
 	return (0);
 }
