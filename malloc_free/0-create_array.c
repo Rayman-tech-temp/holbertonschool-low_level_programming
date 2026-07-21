76590766e1ee14f03c8b7622f4ae4c1462fc1021
Ray Callegari <14826@holbertonstudents.com>
2026-07-21 01:15:25 +0000

    task 0 function file, read me file and main header

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+/**
+ * create_array - creating and populating an array with specified
+ * character passed in arguments.
+ * @size: specified size of array which cannot be negative.
+ * @c: specified character to populate the array with.
+ *
+ * Return: char *array - a pointer array that can be accessed by pointers
+ * allowing to alter the memory's stored value
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *array[size];
+
+	for (int i = 0; i <= size - 1; i++)
+	{
+		array[size] = c;
+	}
+	return (array);
+}
