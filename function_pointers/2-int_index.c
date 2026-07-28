1a4beccf69e5618ec298c2d45104a222340a3c73
Ray Callegari <14826@holbertonstudents.com>
2026-07-28 06:40:50 +0000

    fixed include and typo error

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 
 /**
  * int_index - cycles through an array until a a function
@@ -14,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int (*funct)(int);
 	int i;
 
-	if (size <= 0 || array == NULL || cmp = NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	funct = cmp;
 	i = 0;
