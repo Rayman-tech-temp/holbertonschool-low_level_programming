a9ebaa358c8d6df13130addf9ef28c1f10a679d9
Ray Callegari <14826@holbertonstudents.com>
2026-08-01 05:29:04 +0000

    book marked progress for task 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_strings - variadic function printing arguments.
+ * @separator: the character used to seperate the entries.
+ * @n: number of items to print.
+ *
+ * Return: nothing as this only executes print line outputs.
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arguments;
+	unsigned int i;
+	char *item;
+
+	i = 0;
+	va_start(arguments, n);
+	while (i < n)
+	{
+		item = va_arg(arguments, char *);
+		if (item != NULL)
+		{
+			printf("%s", item);
+				if (i < n - 1 && separator != NULL)
+				printf("%s", separator);
+		} else if (item == NULL)
+			printf("(nil)");
+		i = i + 1;
+	}
+	printf("\n");
+	va_end(arguments);
+}
