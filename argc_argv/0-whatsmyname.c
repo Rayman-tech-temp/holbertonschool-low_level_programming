f8d9149a8bf4d4117ed133b7483729b97921625a
Ray Callegari <14826@holbertonstudents.com>
2026-08-01 08:10:34 +0000

    fixed style and doc in code

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,12 +5,12 @@
  * main - simple execution of argc and argv
  * parameters.
  * @argc: the count of items on the commandline.
- * @argv[]: an array of the items on the commandline.
+ * @argv: an array of the items on the commandline.
  * Return: 0 - there is no fail state.
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc-1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
