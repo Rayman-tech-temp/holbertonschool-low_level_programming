16074915ede6e2837128e83f0945ccf075f499c1
Ray Callegari <14826@holbertonstudents.com>
2026-08-08 13:11:23 +0000

    further case handling for task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -17,12 +17,15 @@ size_t print_list(const list_t *h)
 
 	count = 0;
 	temp = malloc(sizeof(list_t));
-
-	if (h->str == NULL)
+	if (h == NULL)
+		return (count);
+	else if (h->str == NULL)
 		printf("[0] (nil)\n");
 	else
 		printf("[%d] %s\n", h->len, h->str);
 	count = count + 1;
+	if (h->next == NULL)
+		return (count);
 	temp->len = h->next->len;
 	temp->str = h->next->str;
 	temp->next = h->next;
