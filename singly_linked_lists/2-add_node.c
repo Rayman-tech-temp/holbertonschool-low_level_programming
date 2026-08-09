532d806b11edf77219dbbea8e06cf956127aa4f2
Ray Callegari <14826@holbertonstudents.com>
2026-08-09 10:05:23 +0000

    now in style and doc standard

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * add_node - adding new item to the linked list
+ * @head: the function variable passed into this
+ * @str: the value the node will store
+ * return: new_node - returns a pointer to the new
+ * node.
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
@@ -14,7 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 	*head = new_node;
 
 	i = 0;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		i = i + 1;
 	}
