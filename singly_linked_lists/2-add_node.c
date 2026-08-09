ee7579479be3199d1ad0e4546eaa5b4425d33bcd
Ray Callegari <14826@holbertonstudents.com>
2026-08-09 09:35:51 +0000

    eliminated use of strlen use

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,14 +4,20 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *dupe;
+	int i;
 
 	dupe = strdup(str);
 	new_node = malloc(sizeof(list_t));
 
 	new_node->str = dupe;
-	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
+	i = 0;
+	while(str[i] != '\0')
+	{
+		i = i + 1;
+	}
+	new_node->len = i;
 	return (new_node);
 }
