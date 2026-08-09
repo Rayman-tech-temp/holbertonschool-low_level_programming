85f70024ddbbf97ffe427d957e895886fcf5c5cf
Ray Callegari <14826@holbertonstudents.com>
2026-08-09 10:22:27 +0000

    handling failed malloc execution

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,7 +15,10 @@ list_t *add_node(list_t **head, const char *str)
 	int i;
 
 	dupe = strdup(str);
+
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->str = dupe;
 	new_node->next = *head;
