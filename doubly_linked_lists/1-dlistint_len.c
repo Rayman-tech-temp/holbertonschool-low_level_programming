54fd7c7dac9a0c3d6014d894777128df56e8afef
Ray Callegari <14826@holbertonstudents.com>
2026-08-10 23:59:12 +0000

    submitting code for task 1 take one

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * print_dlistint_len - a function that explores the
+ * the list through the next links.
+ * @h: the head of the list to go down.
+ * Return: count - a size_t variable of the
+ * number of elements in the list.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+	dlistint_t *current;
+
+	count = 0;
+	if (h != NULL)
+	{
+		current = malloc(sizeof(dlistint_t));
+		if (current == NULL)
+			return (count);
+
+		current->next = h->next;
+		count = count + 1;
+		if (current->next != NULL)
+		{
+			while (current->next != NULL)
+			{
+				count = count + 1;
+				current = current->next;
+			}
+		}
+	}
+	return (count);
+}
