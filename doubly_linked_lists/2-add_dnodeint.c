87f2eacb001e832f57ae8e625fb70c5f05116b9b
Ray Callegari <14826@holbertonstudents.com>
2026-08-11 02:22:04 +0000

    ensuring head is able to point both ways

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		current->next = *head;
 		current->n = n;
 		*head = current;
-		(*head)->prev = current->next;
+		(*head)->prev = NULL;
 		current->prev = NULL;
 	} else
 	{
