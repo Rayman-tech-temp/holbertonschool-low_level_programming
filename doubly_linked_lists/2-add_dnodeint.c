2616ec9b43a55aa471d553dbd0fbc81c92306c34
Ray Callegari <14826@holbertonstudents.com>
2026-08-11 04:08:24 +0000

    correcting errors in code

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (current == NULL)
 		return (NULL);
 
-	if (head != NULL)
+	if (*head != NULL)
 	{
 		current->next = *head;
 		*head = current;
