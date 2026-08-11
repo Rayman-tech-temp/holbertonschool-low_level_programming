a145c6ab37e3747122d9182c28acce4712e6216d
Ray Callegari <14826@holbertonstudents.com>
2026-08-11 02:25:35 +0000

    making sure to assign both links to first list item

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		current->next = *head;
 		current->n = n;
 		*head = current;
-		(*head)->prev = NULL;
+		(*head)->prev = current;
 		current->prev = NULL;
 	} else
 	{
