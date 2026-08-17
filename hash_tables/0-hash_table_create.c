8074380511d8fa2929d87e6d863d5596c6c99d08
Ray Callegari <14826@holbertonstudents.com>
2026-08-17 03:36:17 +0000

    attmepting task 0 empty hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function to create a
+ * hash table with memory allocated.
+ * @size: the number of elements in the array
+ *
+ * Return: hash_table_t - an empty hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *tableTop;
+	hash_node_t **newArray;
+
+	tableTop = malloc(size);
+	if (tableTop == NULL)
+		return (NULL);
+
+	newArray = malloc(sizeof(hash_node_t));
+	if (newArray == NULL)
+		return (NULL);
+
+	tableTop->size = size;
+	tableTop->array = newArray;
+
+	return (tableTop);
+}
