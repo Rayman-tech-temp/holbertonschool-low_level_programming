4dfe94d35235b569d478f667d6f318879eceac8b
Ray <14826@holbertonstudents.com>
2026-08-21 22:37:02 +1000

    covering additioanl cases and fitting in style.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * read_textfile -
+ * read_textfile - a program the accepts file inputs and
+ * prints them to terminal and shows the count.
  * @filename: a char array index to start the read process
  * @letters: how many characters to print.
  * Return: ssize_t - a signed integer (-1, to SSIZE_MAX).
@@ -12,6 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, count;
 
 	fd = 0;
+	count = 0;
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
@@ -19,10 +21,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename != NULL)
 	{
 		fd = open(filename, O_RDONLY);
+		if (fd == -1)
+			return (count);
+
 		count = read(fd, buffer, letters);
 		count = write(1, buffer, count);
 		close(fd);
-		return (count);
-	} else
-		return (0);
+	}
+
+	return (count);
 }
