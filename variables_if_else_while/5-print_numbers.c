0843a1112ab18781f2bbf410284594633c1f2c9e
Ray <14826@holbertonstudents.com>
2026-09-14 08:57:33 +1000

    submitting task 5 in style and doc

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - a while loop the sequentially increments digits from
+ * 0 to 9 on one line followed by a new line.
+ *
+ * Return: 0 as this is an exercise to show outputs will always
+ * return a success.
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit != 10)
+	{
+		printf("%d",digit);
+		digit++;
+	}
+
+	putchar('\n');
+	return (0);
+}
