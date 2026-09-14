d95f1d4bcbb6135446aec39aee5f8ff8e2d64eab
Ray <14826@holbertonstudents.com>
2026-09-14 15:37:46 +1000

    submitting task 3 in style and doc

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks if it's upper or lowercase
+ * @c: character to inspect.
+ * Return: int - 1 for lowercase and 0 for otherwise.
+ */
+int _islower(int c)
+{
+	int output;
+
+	output = -1;
+	if (c >= 65 && c <= 90)
+		output = 0;
+
+	if (c >= 97 && c <= 122)
+		output = 1;
+
+	return (output);
+}
