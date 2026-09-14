8d3de2659792aeba218112e6dc09cd658e6ed0c1
Ray <14826@holbertonstudents.com>
2026-09-14 13:15:19 +1000

    submitting code for task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet with ascii
+ * code and _putchar function.
+ * Return: void - the result is put into the terminal.
+ */
+void print_alphabet(void)
+{
+	int letter = 97;
+
+	while (letter < 123)
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+}
