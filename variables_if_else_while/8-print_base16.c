c21588a70429b40a532c6b7ca626b12cc5a2e4b3
Ray <14826@holbertonstudents.com>
2026-09-14 10:09:40 +1000

    submitting task 8 in style and doc

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - a while loop that increments through the ASCII table
+ * for the digits of base 16 number system, through a series of
+ * conditions to skip to another section of the table.
+ *
+ * Return: 0 as this is an exercise to show outputs will always
+ * return a success.
+ */
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit != 103)
+	{
+		if (digit <= 57)
+		{
+			putchar(digit);
+			digit++;
+		} else if (digit == 58)
+		{
+			digit = 97;
+			putchar(digit);
+			digit++;
+		} else
+		{
+			putchar(digit);
+			digit++;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
