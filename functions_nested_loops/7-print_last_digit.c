2746ab5be9ac23b581a51d90578730b1ef1d7b94
Ray <14826@holbertonstudents.com>
2026-09-14 17:19:24 +1000

    submitting task 7 in style and doc

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns the last decimal digit
+ * from a given number
+ * @n: a given number to find the last digit
+ * Return: int - last digit from given number.
+ */
+int print_last_digit(int n)
+{
+	char array[] = {'0','1','2','3','4','5','6','7','8','9'};
+	int temp;
+
+	if (n < 0)
+		n = n * -1;
+
+	temp = n % 10;
+	_putchar(array[temp]);
+
+	return (temp);
+}
