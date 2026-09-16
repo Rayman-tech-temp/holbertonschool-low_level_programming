7c243f541f52c5ed7a13056e78cb0b2540c5185b
Ray <14826@holbertonstudents.com>
2026-09-16 11:25:20 +1000

    removed duplicate code

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,23 +7,18 @@
 
 void jack_bauer(void)
 {
-	int minutes, hours, tensminute, tenshour;
+	int minutes = 48, hours = 48, tensminute = 48, tenshour = 48;
 
-	tensminute = 48;
-	minutes = 48;
-	tenshour = 48;
-	hours = 48;
 	while (tenshour <= 50)
 	{
 		while (tensminute < 54 && tensminute >= 48)
 		{
 			_putchar(tenshour);
 			_putchar(hours);
-			_putchar(':');
+			_putchar(58);
 			_putchar(tensminute);
 			_putchar(minutes);
 			_putchar('\n');
-
 			minutes = minutes + 1;
 			if (minutes > 57)
 			{
@@ -36,21 +31,13 @@ void jack_bauer(void)
 			tenshour = tenshour + 1;
 			hours = 48;
 		}
-
 		tensminute = 48;
 		minutes = 48;
 		hours = hours + 1;
-
 		if (hours > 57)
 		{
 			hours = 48;
 			tenshour = tenshour + 1;
-			_putchar(tenshour);
-			_putchar(hours);
-			_putchar(':');
-			_putchar(tensminute);
-			_putchar(minutes);
-			_putchar('\n');
 		}
 	}
 }
